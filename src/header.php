dc7177ed7dbda3cc2e1d6d39f46533eda752e890
rgcouk <hello@rickygreen.co.uk>
2016-12-08 21:50:54 +0000

    Update
    Added Function.php, page.php and fix added WP tags

--- src/header.php
+++ src/header.php
@@ -4,13 +4,35 @@
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
 
-        <title>Title</title>
+        <title><?php wp_title(''); ?></title>
 
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 
-        <link rel="stylesheet/less" href="assets/css/app.less" />
+        <link rel="stylesheet/less" href="<?php echo get_template_directory_uri(); ?>/assets/css/app.less" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
         <?php wp_head(); ?>
     </head>
-    <body>
+    <body <?php body_class(); ?>>
+
+      <!-- Fixed navbar -->
+          <nav id="header" class="navbar navbar-fixed-top">
+              <div id="header-container" class="container navbar-container">
+                  <div class="navbar-header">
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                          <span class="sr-only">Toggle navigation</span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                      </button>
+                      <a id="brand" class="navbar-brand" href="#"><?php get_bloginfo( 'name' ); ?></a>
+                  </div>
+                  <div id="navbar" class="collapse navbar-collapse">
+                      <ul class="nav navbar-nav">
+                          <li class="active"><a href="#">Home</a></li>
+                          <li><a href="#about">About</a></li>
+                          <li><a href="#contact">Contact</a></li>
+                      </ul>
+                  </div><!-- /.nav-collapse -->
+              </div><!-- /.container -->
+          </nav><!-- /.navbar -->
--- src/page.php
+++ src/page.php
@@ -4,4 +4,15 @@
  */
 get_header(); ?>
 
+<section id="page-<?= the_ID(); ?>">
+  <header>
+    <h2><?= the_title(); ?></h2>
+  </header>
+  <div class="content">
+    <?php while ( have_posts() ) : the_post(); ?>
+      <?php the_content(); ?>
+    <?php endwhile; // end of the loop. ?>
+  </div>
+</section>
+
 <?php get_footer(); ?>
